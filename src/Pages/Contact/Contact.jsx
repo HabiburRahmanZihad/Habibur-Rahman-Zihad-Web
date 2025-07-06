120f321884870f4b0cf43f514f86debff89e12e8
Habibur Rahman Zihad <e241024@ugrad.iiuc.ac.bd>
2025-07-06 12:18:32 +0600

    Fix vite key

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -94,7 +94,7 @@ const Contact = () => {
 
         const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID;
         const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-        const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+        const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC;
 
 
         const templateParams = {
